48f3b874ae933c04fe429b4672ab18f433537f0b
trungdan <trungdang@silverlakeaxis.com>
2023-05-12 15:04:57 +0700

    feat: add new User without photos and password encoding

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,24 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 
 }
