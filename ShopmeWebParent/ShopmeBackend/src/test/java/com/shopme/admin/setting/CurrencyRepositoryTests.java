560c27748fe393395d935909921f779630dc5778
trungdan <trungdang@silverlakeaxis.com>
2023-05-31 00:22:42 +0700

    feat: code Setting Entity, Currency Entity and Unit Tests

---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/setting/CurrencyRepositoryTests.java
@@ -0,0 +1,45 @@
+package com.shopme.admin.setting;
+
+import com.shopme.common.entity.Currency;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Arrays;
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+class CurrencyRepositoryTests {
+
+    @Autowired
+    CurrencyRepository currencyRepository;
+
+    @Test
+    void testCreateCurrencies() {
+        List<Currency> currencies = Arrays.asList(
+                new Currency("United States Dollar", "$", "USD"),
+                new Currency("British Pound", "£", "GPB"),
+                new Currency("Japanese Yen", "¥", "JPY"),
+                new Currency("Euro", "€", "EUR"),
+                new Currency("Russian Ruble", "₽", "RUB"),
+                new Currency("South Korean Won", "₩", "KRW"),
+                new Currency("Chinese Yuan", "¥", "CNY"),
+                new Currency("Brazilian Real", "R$", "BRL"),
+                new Currency("Australian Dollar", "$", "AUD"),
+                new Currency("Canadian Dollar", "$", "CAD"),
+                new Currency("Vietnamese đồng ", "₫", "VND"),
+                new Currency("Indian Rupee", "₹", "INR")
+        );
+
+        currencyRepository.saveAll(currencies);
+
+        Iterable<Currency> currencyIterable = currencyRepository.findAll();
+
+        Assertions.assertThat(currencyIterable).size().isEqualTo(12);
+    }
+}
---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/setting/SettingRepositoryTests.java
@@ -0,0 +1,46 @@
+package com.shopme.admin.setting;
+
+import com.shopme.common.entity.Setting;
+import com.shopme.common.entity.SettingCategory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+class SettingRepositoryTests {
+
+    @Autowired
+    SettingRepository settingRepository;
+
+    @Test
+    void testCreateGeneralSettings() {
+//        Setting siteName = new Setting("SITE_NAME", "Shopme", SettingCategory.GENERAL);
+        Setting siteLogo = new Setting("SITE_LOGO", "Shopme.png", SettingCategory.GENERAL);
+        Setting copyright = new Setting("COPYRIGHT", "Copyright (C) 2023 Shopme Ltd.", SettingCategory.GENERAL);
+
+        settingRepository.saveAll(List.of(siteLogo, copyright));
+
+        Iterable<Setting> settings = settingRepository.findAll();
+
+        Assertions.assertThat(settings).size().isPositive();
+    }
+
+    @Test
+    void testCreateCurrencySettings() {
+        Setting currencyId = new Setting("CURRENCY_ID", "1", SettingCategory.CURRENCY);
+        Setting symbol = new Setting("CURRENCY_SYMBOL", "$", SettingCategory.CURRENCY);
+        Setting symbolPosition = new Setting("CURRENCY_SYMBOL_POSITION", "before", SettingCategory.CURRENCY);
+        Setting decimalPointType = new Setting("DECIMAL_POINT_TYPE", "POINT", SettingCategory.CURRENCY);
+        Setting decimalDigits = new Setting("DECIMAL_DIGITS", "2", SettingCategory.CURRENCY);
+        Setting thousandsPointType = new Setting("THOUSANDS_POINT_TYPE", "COMMA", SettingCategory.CURRENCY);
+
+        settingRepository.saveAll(List.of(currencyId, symbol, symbolPosition, decimalPointType, decimalDigits, thousandsPointType));
+    }
+}
