9fae7bc6c35f1d82d6d0f4e858194c918a0a3afe
trungdan <trungdang@silverlakeaxis.com>
2023-05-30 14:38:37 +0700

    feat: code Listing Categories

---
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/category/CategoryService.java
@@ -0,0 +1,31 @@
+package com.shopme.category;
+
+import com.shopme.common.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> listNoChildrenCategories() {
+        List<Category> listNoChildrenCategories = new ArrayList<>();
+        List<Category> listEnabledCategories = categoryRepository.findAllEnabled();
+
+        listEnabledCategories.forEach(category -> {
+            Set<Category> children = category.getChildren();
+
+            if (null == children || children.isEmpty()) {
+                listNoChildrenCategories.add(category);
+            }
+        });
+
+        return listNoChildrenCategories;
+    }
+}
--- ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/site/MainController.java
+++ ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/site/MainController.java
@@ -1,13 +1,25 @@
 package com.shopme.site;
 
+import com.shopme.category.CategoryService;
+import com.shopme.common.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("")
-    public String viewHomePage() {
+    public String viewHomePage(Model model) {
+        List<Category> categories = categoryService.listNoChildrenCategories();
+
+        model.addAttribute("categories", categories);
         return "index";
     }
 }
